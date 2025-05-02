8745c9e63d49d233b4a36e6428e72f1d82845f47
t-hirata74 <t.hirata.0463@gmail.com>
2025-05-02 16:19:51 +0900

    Bannerコンポーネントの実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import { requests } from "./request";
 import { Row } from "./components/Row";
+import { Banner } from "./components/Banner";
 
 function App() {
   return (
     <div className="App">
-     <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Banner />
+      <Row
+        title="NETFLIX ORIGUINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="News Movies" fetchUrl={requests.fetchNewsMovies} />
+      <Row title="Kids Movies" fetchUrl={requests.fetchKidsMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentMovies} />
     </div>
   );
 }
